5a51c585b68a48bfe51a275fa149b75d82ed4f5f
Brian Mayo <ioprotium@gmail.com>
2021-12-04 16:12:54 +0100

    feat: added algo-cpp

---
+++ packages/algo-cpp/algo_00.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+#include <unordered_map>
+using namespace std;
+
+int most_frequent(int arr[], int n)
+{
+  int max_count = 0, res = -1;
+  unordered_map<int, int> hash;
+  for (int i = 0; i < n; i++)
+  {
+    hash[arr[i]]++;
+    if (max_count < hash[arr[i]])
+    {
+      res = arr[i];
+      max_count = hash[arr[i]];
+    }
+  }
+
+  return res;
+}
+
+TEST(MostFrequent, FindsMostFrequent)
+{
+  int input[5] = {0, 1, 2, 1, 4};
+  EXPECT_EQ(most_frequent(input, 5), 1);
+}
