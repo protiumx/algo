5d7aa638a01a2a80fe1e5c65048f48c13ee6207c
Brian Mayo <ioprotium@gmail.com>
2021-12-09 14:49:16 +0100

    refactor: improve go solution code

--- packages/algo-go/solutions/algo_02.go
+++ packages/algo-go/solutions/algo_02.go
@@ -11,20 +11,13 @@ func NewTreeNode(val int) *TreeNode {
 }
 
 func IsValidBST(root *TreeNode, min *int, max *int) bool {
-	if min != nil && root.val <= *min {
+	if root == nil {
+		return true
+	}
+	if (min != nil && root.val <= *min) ||
+		(max != nil && root.val >= *max) {
 		return false
 	}
-	if max != nil && root.val >= *max {
-		return false
-	}
-	leftIsValid := true
-	rightIsValid := true
 
-	if root.left != nil {
-		leftIsValid = IsValidBST(root.left, min, &root.val)
-	}
-	if root.right != nil {
-		rightIsValid = IsValidBST(root.right, &root.val, max)
-	}
-	return leftIsValid && rightIsValid
+	return IsValidBST(root.left, min, &root.val) && IsValidBST(root.right, &root.val, max)
 }
